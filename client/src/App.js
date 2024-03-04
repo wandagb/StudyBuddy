0a47096bca230f3bbd2a9115e1ddc77d48e701f6
Mark Diaz <mldiaz2004@gmail.com>
2024-03-03 21:25:48 -0800

    Added create flashcard-set feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { FlashCardPage } from './pages/FlashcardPage';
+import { FlashCardPage } from './pages/flashcardPage';
 import { Home } from './pages/Home';
 import { LoginPage } from './pages/LoginPage';
 import { FlashcardSetPage } from './pages/FlashSet';
+import {SetForm} from './pages/Create'
 import Navbar from './components/Navbar';
 
 function App(){
@@ -11,7 +12,8 @@ function App(){
     <Router>
         <Navbar />
         <Routes>
-            <Route path="/view-flashcard" element={<FlashCardPage />} />                  
+            <Route path="/view-flashcard" element={<FlashCardPage />} />   
+            <Route path="/create" element={<SetForm />} />                                 
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<LoginPage />} /> 
             <Route path="/set/:setID" element={<FlashcardSetPage />} />                    
---
+++ client/src/pages/Create.js
@@ -0,0 +1,75 @@
+
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import "../App.css";
+import '../components/submitButton.css';
+import '../components/create.css';
+
+
+// Create form, create a new set
+export const SetForm = () => {
+
+    // const [flashSet, setFlashSet] = useState([]);
+    const [name, setName] = useState('');
+    // const [error, setError] = useState(null);
+    const navigate = useNavigate(); // Initialize useHistory hook
+
+
+    // Hardcoded UserID from database
+    // const userID = "65e256f05ca22e1f4b545aa3"
+
+    const handleSubmit = async (e) =>{
+        navigate('/home');
+
+        e.preventDefault();
+        
+        const set = {name}
+
+        const response = await fetch('/api/set', {
+            method: 'POST',
+            body: JSON.stringify(set),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        });
+
+        const json = await response.json();
+
+        if(!response.ok){
+            // setError({error: 'Error'}); 
+        }
+
+        if(response.ok){
+            // setError(null);
+            setName('')
+            console.log('New set added!', json);
+        }
+    }    
+    // redirect: reference home.js
+    return (
+        <>
+        <div className='wrapper-main'>
+            <div className='create-container'>
+                <h1>Create</h1>
+                    <form className='Create' onSubmit={handleSubmit}>
+                        <h3> Name your set</h3>
+
+                        <label>Set name:</label>
+                        <input className = 'search-box'
+                            type='text'
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}
+                        />
+                        <button className='submit-button'>
+                            submit
+                        </button>
+                        {/* {error && <div className="error"></div>} */}
+                    </form>
+            </div>
+        </div>
+
+        </>
+
+    );
+}
