d009f324ba87bb6ff2136a960bbccdf9251e98ba
Wanda <wbarahona@g.ucla.edu>
2024-03-07 22:33:07 -0800

    Finish login and sign up validations

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -47,4 +47,23 @@ userSchema.statics.signup = async function(email, username, password) {
     return user
 }
 
+userSchema.statics.login = async function(email, password){
+    if(!email ||! password) {
+        throw Error('All fields must be filled')
+    }
+        const user = await this.findOne({email})
+
+        if (!user){
+            throw Error('Incorrect email')
+        }
+
+        const match = await bcrypt.compare(password, user.password)
+
+        if (!match) {
+            throw Error ('Incorrect password')
+        }
+
+        return user
+    }
+
 module.exports = mongoose.model('User', userSchema)
