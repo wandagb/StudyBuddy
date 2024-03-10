3c87c56e7e360cbd40c16173eb9489a0778028cc
Andrew <andrewrubio@ucla.edu>
2024-03-10 16:09:09 -0700

    Delete route and dispatch functonality (not working)

--- client/src/components/flashcard.js
+++ client/src/components/flashcard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Flashcard.css";
+<<<<<<< Updated upstream
 
 export default function Card(props) {
     const [isFront, changeFace] = React.useState(true);
@@ -10,6 +11,41 @@ export default function Card(props) {
     const text = isFront ? props.frontSide :props.backSide;
     const sideClass = isFront ? "front" : "back";
     const classList = `flash-card ${sideClass}`;
+=======
+import { useCardsContext } from '../hooks/useCardsContext';
+import { useAuthContext } from "../hooks/useAuthContext";
+import { FlashcardSetPage } from "../pages/FlashSet"; 
+
+export default function Card(props) {
+    const [isFront, changeFace] = React.useState(true);
+
+    const { card_id } = props.card_id
+
+    const { cardDispatch} = useCardsContext();
+
+    function handleClick() {
+        changeFace(oldState => !oldState); }
+
+    const handleDelete = async () => {
+
+        console.log(props.card_id)
+
+        const response = await fetch('/api/items/flashcard' + card_id, {
+            method: 'DELETE'
+        });
+
+        const json = await response.json()
+        
+        if (response.ok){
+            cardDispatch({type: 'DELETE_CARD', payload: json})
+            }
+     }
+
+     const text = isFront ? props.frontSide :props.backSide;
+     const sideClass = isFront ? "front" : "back";
+     const classList = `flash-card ${sideClass}`;
+    
+>>>>>>> Stashed changes
     return (
         <div className={classList} onClick={handleClick}>
             {text}
