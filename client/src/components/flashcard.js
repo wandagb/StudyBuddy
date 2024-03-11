3e9ad959ea9cf4bfded09bad7bc4300696222768
Ted Smith <tedsmith@Teds-MacBook-Pro.local>
2024-03-10 17:01:25 -0700

    Updated styling for flashcards
    Edited Flashcard.css and flashcard.js to add a flipping animation to the cards.

--- client/src/components/flashcard.js
+++ client/src/components/flashcard.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Flashcard.css";
 
-export default function Card(props) {
-    const [isFront, changeFace] = React.useState(true);
+export default function Card({ frontSide, backSide }) {
+    const [isFlipped, setIsFlipped] = useState(false);
+
     function handleClick() {
-        changeFace(oldState => !oldState);
+        setIsFlipped(!isFlipped);
     }
-    
-    const text = isFront ? props.frontSide :props.backSide;
-    const sideClass = isFront ? "front" : "back";
-    const classList = `flash-card ${sideClass}`;
+
     return (
-        <div className={classList} onClick={handleClick}>
-            {text}
+        <div className="flash-card-container" onClick={handleClick}>
+            <div 
+              className="flash-card" 
+              style={{transform: isFlipped ? "rotateY(180deg)" : "rotateY(0deg)"}}>
+                <div className="flash-card-front">
+                    {frontSide}
+                </div>
+                <div className="flash-card-back">
+                    {backSide}
+                </div>
+            </div>
         </div>
     );
 }
